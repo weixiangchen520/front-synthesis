1091811e3dd4c4a51fa15bc09adcf9848adf717f
weixiangchen520 <1981468862@qq.com>
2022-03-22 20:16:14 +0800

    feat: first demo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import styles from './index.less';
+import 'antd/dist/antd.css';
 
 export default function IndexPage() {
+  const a = 1;
   return (
     <div>
       <h1 className={styles.title}>Page index</h1>
