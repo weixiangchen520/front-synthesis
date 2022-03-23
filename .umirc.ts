e8d69ba2988e8d86ab31a4a3a505f16cd52e8b83
weixiangchen520 <1981468862@qq.com>
2022-03-23 21:59:41 +0800

    feat: 调整菜单

--- .umirc.ts
+++ .umirc.ts
@@ -4,15 +4,52 @@ export default defineConfig({
   nodeModulesTransform: {
     type: 'none',
   },
+  mfsu: {
+    development: {
+      output: './.mfsu-dev',
+    },
+    production: {
+      output: './.mfsu-prod',
+    },
+  },
   fastRefresh: {},
   layout: {
     name: 'WeiXiang Chen',
     locale: true,
-    layout: 'side',
-    navTheme: 'light',
+    layout: 'mix',
+    navTheme: 'dark',
+    headerTheme: 'dark',
+    splitMenus: true,
   },
   routes: [
-    { path: '/', name: '首页', icon: 'windows', component: './index' },
-    { path: '/login', name: '登录', icon: 'windows', component: './login' },
+    {
+      path: '/',
+      name: '首页',
+      icon: 'windows',
+      component: './index',
+      exact: true,
+    },
+    {
+      path: '/examples',
+      name: '案例',
+      icon: 'windows',
+      routes: [
+        {
+          path: '/examples/home',
+          name: '样例',
+          icon: 'windows',
+          component: './examples',
+          exact: true,
+        },
+        {
+          path: '/examples/login',
+          name: '登录',
+          icon: 'windows',
+          component: './login',
+          exact: true,
+        },
+        { redirect: '/examples/home' },
+      ],
+    },
   ],
 });
