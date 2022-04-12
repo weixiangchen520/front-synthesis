50e6852b37c61f64c7f52c0186f07d4d33bf8d3f
weixiangchen520 <1981468862@qq.com>
2022-04-12 21:54:25 +0800

    feat: iframe控制

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,36 @@
 import styles from './index.less';
 import 'antd/dist/antd.css';
 
+import { Button } from 'antd';
+import { FullscreenOutlined, FullscreenExitOutlined } from '@ant-design/icons';
+import { useState } from 'react';
+
 export default function IndexPage() {
+  const [fullScreen, setFullScreen] = useState<boolean>(false);
   return (
-    <div className={styles.wrapper}>
-      <iframe className={styles.iframe} src={'https://f2e.tech/'}></iframe>
+    <div id="home_iframe">
+      <div className={fullScreen ? styles.btnFullScreen : styles.btn}>
+        <Button
+          type="primary"
+          ghost
+          onClick={() => {
+            const target = document.querySelector('#home_iframe');
+            if (fullScreen) {
+              document?.exitFullscreen();
+            } else {
+              target?.requestFullscreen();
+            }
+            setFullScreen((prev) => !prev);
+          }}
+        >
+          {fullScreen ? <FullscreenExitOutlined /> : <FullscreenOutlined />}
+        </Button>
+      </div>
+      <iframe
+        allow="fullscreen"
+        className={fullScreen ? styles.iframeFullScreen : styles.iframe}
+        src={'https://f2e.tech/'}
+      />
     </div>
   );
 }
